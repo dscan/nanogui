af2148066b4804bfd5dff07f75e672e490098ab2
Stephen McDowell <svenevs.dev@gmail.com>
2020-01-23 06:24:33 -0500

    ok so then this should fail right?

--- cmake/NanoGUI/Developer.cmake
+++ cmake/NanoGUI/Developer.cmake
@@ -1,17 +1,30 @@
 # This file is included with -DNANOGUI_DEV=ON, do not use this in parent
 # projects.  Compile warnings on, where warnings are errors.
+if (NANOGUI_HAS_PARENT OR NANOGUI_INSTALL)
+  # Flags end up in the *public* interface, which is bad for all usage cases
+  # other than testing NanoGUI packaging / examples.
+  # NOTE: do not WARNING here, we fail CI builds on CMake warnings.
+  message(
+    "(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)\n"
+    "(!) NANOGUI_DEV=ON but NanoGUI has a parent or is being installed.   (!)\n"
+    "(!) This should *ONLY* be done if you are developing NanoGUI.        (!)\n"
+    "(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)(!)\n"
+  )
+endif()
+
 if (MSVC)
   # /W3 may be added by default, remove it in favor of /W4.
   if (CMAKE_CXX_FLAGS MATCHES "/W[0-4]")
     string(REGEX REPLACE "/W[0-4]" "" CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
   endif()
-  target_compile_options(nanogui-private-interface INTERFACE /W4 /WX)
+  target_compile_options(nanogui-interface INTERFACE /W4 /WX)
+  target_link_options(nanogui-interface INTERFACE /WX)
 else()
   # Clang, GCC, Intel, etc.
   foreach (flag -Wall -Wextra -Werror)
     check_cxx_compiler_flag(${flag} have_flag)
     if (have_flag)
-      target_compile_options(nanogui-private-interface INTERFACE ${flag})
+      target_compile_options(nanogui-interface INTERFACE ${flag})
     endif()
   endforeach()
 endif()
